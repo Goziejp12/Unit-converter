f01beaa587f18bbf383810dede104942f20d18a8
Chigozie Johnpaul Maduka <chigoziemaduka401@gmail.com>
2022-11-02 14:48:40 +0100

    commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const convertBtn = document.querySelector("#convertBtn")
+const resetBtn = document.querySelector("#resetBtn")
+const inputEl = document.querySelector("input")
+const lengthEl = document.querySelector("#length")
+const volumeEl = document.querySelector("#volume")
+const massEl = document.querySelector("#mass")
+let htmlPart = "" 
+
+convertBtn.addEventListener("click", function() {
+    lengthConversion()
+    volumeConversion()
+    massConversion()
+})
+
+resetBtn.addEventListener("click", function() {
+    lengthEl.innerHTML += "" 
+})
+
+function lengthConversion() {
+    htmlPart = `<p>${inputEl.value} meter(s) = ${(inputEl.value * 3.281).toFixed(3)} feet | 
+    ${inputEl.value} feet = ${(inputEl.value / 3.281).toFixed(3)} meter(s)</p>`
+    lengthEl.innerHTML += htmlPart
+}
+
+function volumeConversion() {
+    htmlPart = `<p>${inputEl.value} liter(s) = ${(inputEl.value * 0.264).toFixed(3)} gallon(s) | 
+    ${inputEl.value} gallon(s) = ${(inputEl.value / 0.264).toFixed(3)} liters(s)</p>`
+    volumeEl.innerHTML += htmlPart
+}
+
+function massConversion() {
+    htmlPart = `<p>${inputEl.value} kilo(s) = ${(inputEl.value * 2.204).toFixed(3)} pound(s) | 
+    ${inputEl.value} pound(s) = ${(inputEl.value / 2.204).toFixed(3)} kilo(s)</p>`
+    massEl.innerHTML += htmlPart
+}
